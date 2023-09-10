9ff99441602d6e17fdab1538223191b3a98e93ec
Ferran Galan <reachfg@protonmail.com>
2023-09-10 17:27:32 +0200

    reversed version to 0.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(name='crisis_helper',
-      version="0.0.2",
+      version="0.0.1",
       description="crisis_helper (binary_api_pred)",
       license="MIT",
       author="Le Wagon",
