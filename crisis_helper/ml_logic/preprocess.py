b846fec3f28ee1fa247f7c8b87079127b13204fd
Hamzah Baagil <hhbaagil@gmail.com>
2023-09-11 15:43:50 +0200

    pulled from main branch, packages are working locally

--- crisis_helper/ml_logic/preprocess.py
+++ crisis_helper/ml_logic/preprocess.py
@@ -5,11 +5,6 @@ import numpy as np
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-<<<<<<< HEAD
-
-=======
-#from sklearn.feature_extraction.text import TfidfVectorizer
->>>>>>> 428d29570d094aaa926cda93cf3d284a15d2938e
 
 
 def text_cleaning(X: pd.DataFrame):
@@ -42,15 +37,9 @@ def text_cleaning(X: pd.DataFrame):
 
     return cleaned_sentence
 
-<<<<<<< HEAD
 
 """def tfidf_vectorizer():
 
-=======
-'''
-def tfidf_vectorizer():
-    """
->>>>>>> 428d29570d094aaa926cda93cf3d284a15d2938e
     Instantiate TF-IDF vectorizer.
 
 
@@ -59,7 +48,6 @@ def tfidf_vectorizer():
 
     print("✅ feature text with TF-IDF vectorized")
     return vectorizer
-<<<<<<< HEAD
 
 def tfidf_vectorizer_fit(df, vectorizer):
     fit_feature = vectorizer.fit(df)
@@ -70,6 +58,3 @@ def tfidf_vectorizer_transform(df, vectorizer):
     fit_feature = vectorizer.fit(df)
 
     return fit_feature"""
-=======
-'''
->>>>>>> 428d29570d094aaa926cda93cf3d284a15d2938e
